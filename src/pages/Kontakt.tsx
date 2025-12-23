430e9d1ab29a8df412dc0d238738d921af15c192
Patrick-Jaeger <45011971+Patrick-Jaeger@users.noreply.github.com>
2025-12-23 17:53:50 +0100

    Update Kontakt.tsx

--- src/pages/Kontakt.tsx
+++ src/pages/Kontakt.tsx
@@ -235,7 +235,7 @@ const Kontakt = () => {
                                 animate={{ opacity: 1 }}
                                 exit={{ opacity: 0 }}
                               >
-                                <WaveLoader message="senden..." />
+                                <WaveLoader message=" senden..." />
                               </motion.div>
                             )}
                             {buttonState === "success" && (
@@ -247,7 +247,7 @@ const Kontakt = () => {
                                 className="flex items-center"
                               >
                                 <SuccessCheck size="sm" className="mr-2" />
-                                <span className="text-emerald-500 font-bold">Anfrage gesendet</span>
+                                <span className="text-emerald-500 font-bold"> Anfrage gesendet</span>
                               </motion.div>
                             )}
                             {buttonState === "initial" && (
