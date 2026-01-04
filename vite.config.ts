d37328573c3bcf71dc95b0025b241a9fa1ce085f
Patrick Jäger <patrick.jaeger1@gmx.de>
2026-01-04 15:42:01 +0100

    Prepare for root GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/the-end-website/", // Sehr wichtig für GitHub Pages
+  base: "/", // <- Wichtig! Setzt Root-Pfad für GitHub Pages
   server: {
     host: "::",
     port: 8080,
