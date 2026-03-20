b930b115a6f75717b536a4f82fc2189e2b5fc205
Patrick-Jaeger <45011971+Patrick-Jaeger@users.noreply.github.com>
2026-03-20 11:12:38 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,18 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // Fixed MIME type issue
+  base: "/mendorferbuch/",
+
   server: {
     host: "::",
     port: 8080,
+    hmr: {
+      overlay: false,
+    },
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
